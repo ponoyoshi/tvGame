8ad6cf61ff985ccdbaa7ad1cfac16ac7d91c3c4a
tim-sharpe <timsharpe.work@gmail.com>
2020-06-08 19:41:33 +0200

    Fix Shitty merge

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,8 @@ public class GameManager : MonoBehaviour
     [SerializeField] private CandidatesPanelWidget candidatePanelWidget = null;
     public int currentQuestion = 0;
     public int currentMoney = 0;
+    private const int MONEY_MULTIPLE = 50;
+    private const int MONEY_DIVIDE_RATIO = 2;
     public const int MONEY_LOST_PER_MISTAKE = 250;
     public const int MONEY_GAINED_PER_GOOD_PREDICTION = 500;
     [SerializeField] private List<string> questions = new List<string>();
@@ -24,7 +26,12 @@ public class GameManager : MonoBehaviour
     {
         if (incorrectPredictions > 0 && correctPredictions == 0)
         {
-            currentMoney = currentMoney / 2;
+            int roundedMoney = currentMoney / MONEY_DIVIDE_RATIO;
+            roundedMoney /= MONEY_MULTIPLE;
+            roundedMoney = Mathf.RoundToInt(roundedMoney);
+            roundedMoney *= MONEY_MULTIPLE;
+
+            currentMoney = roundedMoney;
         }
         else if (incorrectPredictions > 0)
         {
