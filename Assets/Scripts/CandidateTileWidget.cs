714f8ec486cfe86aaa2c4f28c32e20fa08aa0e3c
tim-sharpe <timsharpe.work@gmail.com>
2020-06-08 21:26:49 +0200

    Add money to Candidates

--- Assets/Scripts/CandidateTileWidget.cs
+++ Assets/Scripts/CandidateTileWidget.cs
@@ -12,6 +12,7 @@ public class CandidateTileWidget : MonoBehaviour
     [SerializeField] private TextMeshProUGUI nameText = null;
     [SerializeField] private TextMeshProUGUI preferencesText = null;
     [SerializeField] private TextMeshProUGUI scoreText = null;
+    [SerializeField] private TextMeshProUGUI moneyText = null;
     [SerializeField] private TextMeshProUGUI answerText = null;
     [SerializeField] private Profile currentCandidate = null;
     [SerializeField] private Animator animator = null;
@@ -23,6 +24,9 @@ public class CandidateTileWidget : MonoBehaviour
     [SerializeField] private Color UPVOTE_COLOR = Color.blue;
     [SerializeField] private Color DOWNVOTE_COLOR = Color.red;
     [SerializeField] private Color DEFAULT_COLOR = Color.grey;
+
+    private const int MONEY_LOST_PER_MISTAKE = 1000;
+    private const int MONEY_GAINED_PER_SUCCESS = 1000;
 
     private void Start()
     {
@@ -37,15 +41,16 @@ public class CandidateTileWidget : MonoBehaviour
     public void SetCurrentCandidate (Profile scriptableObject)
     {
         currentCandidate = scriptableObject;
-        SetTile(currentCandidate.image, currentCandidate.fullName, currentCandidate.interest, currentCandidate.answers[gameManager.currentQuestion].answer, currentCandidate.score);
+        SetTile(currentCandidate.image, currentCandidate.fullName, currentCandidate.interest, currentCandidate.answers[gameManager.currentQuestion].answer, currentCandidate.score, currentCandidate.money);
     }
 
-    private void SetTile(Sprite picture, string name, string preferences, string answer, int score = 0)
+    private void SetTile(Sprite picture, string name, string preferences, string answer, int score, int money)
     {
         profilePicture.sprite   = picture;
         nameText.text           = name;
         preferencesText.text    = preferences;
         scoreText.text          = score.ToString();
+        moneyText.text          = money.ToString();
         answerText.text         = answer;
 
         currentUpvoteScore = 0;
@@ -107,6 +112,7 @@ public class CandidateTileWidget : MonoBehaviour
         if (currentCandidate.answers[gameManager.currentQuestion].isTrue)
         {
             currentCandidate.score += 1;
+            currentCandidate.money += MONEY_GAINED_PER_SUCCESS;
 
             if (currentUpvoteScore == 2)
             {
@@ -125,6 +131,13 @@ public class CandidateTileWidget : MonoBehaviour
         //wrong answer from candidate
         else
         {
+            currentCandidate.money -= MONEY_LOST_PER_MISTAKE;
+
+            if (currentCandidate.money < 0)
+            {
+                currentCandidate.money = 0;
+            }
+
             if (currentUpvoteScore == 2)
             {
                 return 2;
