7f92c3440542000bd469c27cdf993055ec01145e
ponoyoshi <lopzerk@hotmail.fr>
2020-06-08 17:11:06 +0200

    Game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int currentQuestion = 0;
+    public int currentMoney = 0;
+    public void NextQuestion()
+    {
+        currentQuestion++;
+    }
+}
