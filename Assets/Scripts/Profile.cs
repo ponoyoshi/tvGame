59d476aef1424261b2457c281d25744b36d474e3
ponoyoshi <lopzerk@hotmail.fr>
2020-06-08 14:46:32 +0200

    modifications structure SO

--- Assets/Scripts/Profile.cs
+++ Assets/Scripts/Profile.cs
@@ -1,18 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
 
-public class Profile
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Profile", order = 1)]
+public class Profile : ScriptableObject
 {
     public int ID;
     public string fullName;
     public string interest;
-    public Answer[] answers;
+    public List<Answer> answers;
     public int money;
+    public Image image;
+
+    [Serializable]
+    public struct Answer
+    {
+        [TextArea(3,3)]
+        public string answer;
+        public bool isTrue;
+    }
 }
 
-public class Answer
-{
-    public string answer;
-    public bool isTrue;
-}
