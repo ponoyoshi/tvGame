71019b13db361e0b244bbd3e06de3fb40b5731c1
ponoyoshi <lopzerk@hotmail.fr>
2020-06-10 12:07:32 +0200

    ProfileOptions

---
+++ Assets/ProfileOption.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProfileOption : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    public void ModifyImage(Sprite sprite) => image.sprite = sprite;
+}
