fd8c692fac23a25c3040bac32dfbf561abbbe40a
tim-sharpe <timsharpe.work@gmail.com>
2020-06-10 12:08:02 +0200

    Maybe I borke everything

---
+++ Assets/MainMenuProfileBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuProfileBehaviour : MonoBehaviour
+{
+    [SerializeField] private Animator animator = null;
+
+    void Start()
+    {
+        StartCoroutine();
+    }
+
+    private void StartCoroutine()
+    {
+        StartCoroutine(PlayAnimationAfterDelay());
+    }
+
+    private IEnumerator PlayAnimationAfterDelay()
+    {
+        yield return new WaitForSeconds(0.64516f);
+        if (Random.Range(0, 1) >= 0.5)
+        {
+            animator.SetTrigger("Scale");
+        }
+
+    }
+}
